2d53e1e3d0e9470086b9eec15d1851824c8eaebd
DevTabibur <tobiburrohman2@gmail.com>
2022-04-24 02:33:30 +0600

    server loaded and displayed the data

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
 import './Services.css';
@@ -7,7 +8,7 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect( ()=>{
-        fetch('services.json')
+        fetch("http://localhost:5000/service")
         .then(res => res.json())
         .then(data => setServices(data));
     }, [])
@@ -19,7 +20,7 @@ const Services = () => {
             <div className="services-container">
             {
                 services.map(service => <Service
-                    key={service.id}
+                    key={service._id}
                     service={service}
                 >
                 </Service>)
