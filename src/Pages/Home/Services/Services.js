bc6a85e5162004ce980cb6abeaf432f9f060038e
DevTabibur <tobiburrohman2@gmail.com>
2022-04-24 17:08:20 +0600

    data showed

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,11 +1,12 @@
 // @ts-nocheck
 import React, { useEffect, useState } from 'react';
+import useServices from '../../../Hooks/useServices';
 import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
 
-    
+    const [services] = useServices([]);
 
     return (
         <div id="services" className='container'>
--- src/Pages/ManageServices/ManageServices.jsx
+++ src/Pages/ManageServices/ManageServices.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import useServices from '../../Hooks/useServices'
 
 const ManageServices = () => {
+    const[services] = useServices();
   return (
-    <div>ManageServices</div>
+    <div className='w-50 mx-auto'>
+        <h2>Manage Services</h2>
+        {
+            services.map(service => <div key={service._id}>
+                <h5>{service.name} <button>X</button></h5>
+            </div>)
+        }
+    </div>
   )
 }
 
